d2a62a0103475134cc8cecb4a3115b43b6f1d834
Paloma Maciel Pereira dos Santos <paloma.maciel@aluno.ifsertao-pe.edu.br>
2025-11-07 23:19:08 -0300

    Alterações de Paloma do  55min do vídeo  até 1h e 45min

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,4 +10,23 @@ export class Tab2Page {
 
   constructor() {}
 
+  doarAlimentos() {
+    this.abrirUrl (
+    'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+De+Alimento');
+  }
+  
+  doarRoupas() {
+    this.abrirUrl (
+    'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+De+Roupas');
+  }
+
+  doarSangue() {
+    this.abrirUrl ( 
+    'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+De+Sangue');
+  }
+
+  abrirUrl(url: string) {
+    window.open(url,'_blank')
+    ?.focus();
+  }
 }
