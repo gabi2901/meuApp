fb162e688a49a0131094b90b5a0743464fa93ea1
ladylyny <kallyny.victoria@aluno.ifsertao-pe.edu.br>
2025-11-11 22:48:25 -0300

    Atualizações no formulário, botão e imagem receber.png

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  form!: FormGroup;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
+  }
+
+  abrirUrl(url: string) {
+    window.open(url, '_blank')?.focus();
+  }
+
+  enviar() {
+    let nome = this.form.get('nome')?.value;
+    let endereco = this.form.get('endereco')?.value;
+    console.log(nome, endereco);
+
+    this.abrirUrl(
+      `https://api.whatsapp.com/send?phone=5587981217492&text=Olá,%20me%20chamo%20${nome},%20moro%20em%20${endereco}%20e%20vim%20através%20do%20seu%20app.%20Gostaria%20de%20receber%20doações!`
+    );
+  }
 }
